50acf78d9fe6f8681fd3aa75580245100686f537
Luis Fernando <106917465+LuisFernando656@users.noreply.github.com>
2023-01-16 14:36:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
-import {Modal} from './modal.js'
-import {AlertError} from './alert-error.js'
-import {notANumber, calculateIMC} from './utils.js'
+import { Modal } from './modal.js'
+import { AlertError } from "./alert-error.js"
+import { calculateIMC, notANumber } from "./utils.js"
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-form.onsubmit = e => {
-  e.preventDefault()
-  const weight = inputWeight.value
-  const height = inputHeight.value
+form.onsubmit = event => {
+    event.preventDefault()
 
-  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+    const weight = inputWeight.value
+    const height = inputHeight.value
 
-  if(weightOrHeightIsNotANumber){
-    AlertError.open()
-    return
-  }
-  
-  const result = calculateIMC(weight,height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-  displayResult(result)
+    if (weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
 }
 
-function displayResult (result) {
-  const message = `Seu IMC é de ${result}`
-  Modal.message.innerHTML = message
-  Modal.open()
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
 }
 
-inputHeight.oninput = () =>  AlertError.close()
-inputWeight.oninput = () =>  AlertError.close()
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 export function notANumber(value) {
-  return isNaN(value) || value == ""
+    return isNaN(value) || value == ""
 }
 
 export function calculateIMC(weight, height) {
-  return (weight / ((height/100) ** 2)).toFixed(2)
+    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
